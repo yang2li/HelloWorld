72ee3b26cfbd3e3e8a251d94e5920b9b7d8102c7
zhongshenchao <274943730@qq.com>
2017-08-05 10:39:10 +0800

    Initial commit

---
+++ src/main/java/Test02.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by 27494 on 2017/8/5.
+ */
+public class Test02 {
+@Test
+    public void sayHello() {
+    System.out.println("hello world");
+}
+
+
+
+
+}
